2e1a91970a3f19d6e57d0d63eb7814f15f994155
Roushan Raj <roushanrj776@gmail.com>
2021-08-27 09:36:28 +0530

    Pie chart added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxEchartsModule } from 'ngx-echarts';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BarChartComponent } from './bar-chart/bar-chart.component';
 import { PieChartComponent } from './pie-chart/pie-chart.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { FormsModule } from '@angular/forms';
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 
 @NgModule({
@@ -18,7 +21,10 @@ import { PieChartComponent } from './pie-chart/pie-chart.component';
   ],
   imports: [
     BrowserModule,
+    NgApexchartsModule,
+    FormsModule,
     HttpClientModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     NgxEchartsModule.forRoot({
       echarts: () => import('echarts'),
--- src/app/pie-chart/pie-chart.component.ts
+++ src/app/pie-chart/pie-chart.component.ts
@@ -1,17 +1,82 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { ChartComponent } from 'ng-apexcharts';
+
+
+//import { NzMessageService } from 'ng-zorro-antd/message';
+import {
+  ApexNonAxisChartSeries,
+  ApexResponsive,
+  ApexChart
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  responsive: ApexResponsive[];
+  labels: any;
+};
 
 @Component({
   selector: 'app-pie-chart',
   templateUrl: './pie-chart.component.html',
-  styleUrls: ['./pie-chart.component.css']
+  styleUrls: ['./pie-chart.component.css'],
 })
-export class PieChartComponent implements OnInit {
+export class PieChartComponent implements OnInit{
 
+  expense: number[]=[];
+  aspect: string[]=[];
 
-  constructor() { }
+  @ViewChild("chart") chart:any| ChartComponent;
+  public chartOptions: Partial<ChartOptions> |any;
 
-  ngOnInit(): void {  }
+  ngOnInit(): void {
+    this.getExpense();
+  }
+  constructor(private httpclient: HttpClient,private changedetect: ChangeDetectorRef) {
+    this.chartOptions = {
+      series: this.expense,
+      chart: {
+        width: 380,
+        type: "pie"
+      },
+      labels: this.aspect,
+      responsive: [
+        {
+          breakpoint: 480,
+          options: {
+            chart: {
+              width: 200
+            },
+            legend: {
+              position: "bottom"
+            }
+          }
+        }
+      ]
+    };
+  }
 
-  
+  getExpense(){
+    this.httpclient.get<Expense[]>('http://localhost:9876/budget').subscribe(
+      (data: any) => {
+        for(let item of data){
+          console.log(data);
+          this.expense.push(item.expense);
+          this.aspect.push(item.aspect);
+        }
+      
+      }
+    );
+  }
 
+}
+
+export class Expense {
+  constructor(
+    public id: number,
+    public expense: number,
+    public aspect: string,
+   
+  ){}
 }
