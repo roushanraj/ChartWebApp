9cec8d66256707897334ad204136d195ce4f18b4
Roushan Raj <roushanrj776@gmail.com>
2021-08-27 15:47:32 +0530

    Minor fixes in UI

--- src/app/bar-chart/bar-chart.service.spec.ts
+++ src/app/bar-chart/bar-chart.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import {BarChartComponent, Result} from './bar-chart.component'
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
 Result
 describe("BarChartComponent", ()=> 
 {
@@ -17,8 +18,11 @@ describe("BarChartComponent", ()=>
 
     });
     it('gets all the result',()=>{
-        
+        const dummydata: Result[]=[
+            {"name":"Dummy Kumar","rollNo":420,"standard":9,"percentage":99},
+            {"name":"Dummy Kishore","rollNo":421,"standard":9,"percentage":98}
+    ];
+        expect(barservice.names.length).toBe(2);
     });
-
 });
 
